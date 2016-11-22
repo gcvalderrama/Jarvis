741e683db46799d2840dfd46c893980777c1a247
greg <gcvalderrama@hotmail.com>
2016-11-21 23:36:48 -0500

    add rst lemmas

--- Lincoln/03RST/Jarvis/Jarvis/Program.cs
+++ Lincoln/03RST/Jarvis/Jarvis/Program.cs
@@ -10,40 +10,85 @@ namespace Jarvis
     class Program
     {
         private static string PathRoot = @"D:\Tesis2016\Jarvis\Lincoln\03RST\Output";
-        public static void generateEDUS(XElement root)
+        public static IEnumerable<string> generateEDUS(XElement root)
         {
-
-            var files = Directory.GetFiles(PathRoot, "*.txt");
-            foreach (var item in files)
-            {
-                File.Delete(item); 
-            }
-
+            List<string> punc_special = new List<string>() { ",", ".", ";", "?", "%" };
+            List<string> numeric_special = new List<string>() { "$" };
             var query = from c in root.Elements("tokens").Elements("token")
                         select c;
-            int index = 1;
-            StringBuilder sb = new StringBuilder();
-            List<string> result = new List<string>();
+            int index = 1;         
+            List<LinkedList<string>> bag = new List<LinkedList<string>>();
+            LinkedList<string> iter = new LinkedList<string>(); 
             foreach (var token in query)
             {
                 var eduid = int.Parse(token.Attribute("eduidx").Value);
                 if (index != eduid)
                 {
-                    result.Add(sb.ToString()); 
-                    sb = new StringBuilder();
+                    bag.Add(iter);
+                    iter = new LinkedList<string>();
                     index++;
                 }
-                sb.Append(token.Attribute("word").Value + " ");
+                iter.AddLast(token.Attribute("word").Value);
             }
-            result.Add(sb.ToString()); 
+            bag.Add(iter);
 
-            File.WriteAllLines(Path.Combine(PathRoot, string.Format("edus.txt", index )), result.ToArray());            
+            List<string> result = new List<string>();
+            
+            foreach (var item in bag)
+            {
+                var sb = new StringBuilder();
+                for (LinkedListNode<string> it = item.First; it != null;)
+                {
+                    var past = it.Previous;
+                    var next = it.Next;
+
+                    var word = it.Value;
+
+                    if (word == "-LRB-")
+                        word = "(";
+                    if (word == "-RRB-")
+                        word = ")";
+
+                    if (next != null && punc_special.Contains(next.Value))
+                    {
+                        sb.Append(word);
+                    }
+                    else if (numeric_special.Contains(word))
+                    {
+                        sb.Append(word);
+                    }
+                    else if (next != null && next.Value.StartsWith("'"))
+                    {
+                        sb.Append(word);
+                    }
+                    else
+                    {
+                        sb.Append(word + " ");
+                    }
+                    it = next;
+                }
+                result.Add(sb.ToString());
+            }
+            return result; 
+            
         }
 
         static void Main(string[] args)
         {
-            var root = XElement.Load(@"D:\Tesis2016\Jarvis\Lincoln\03RST\Output\rst.xml");
-            generateEDUS(root);
+            var outputpath = @"D:\Tesis2016\Jarvis\Lincoln\03RST\Output";
+            var files = Directory.GetFiles(outputpath, " *.txt");
+            foreach (var item in files)
+            {
+                File.Delete(item);
+            }
+            files = Directory.GetFiles(@"D:\Tesis2016\Jarvis\Lincoln\03RST\Input", "*.jarvis");
+            foreach (var item in files)
+            {
+                var root = XElement.Load(item);
+                var result =  generateEDUS(root);
+                File.WriteAllLines(Path.Combine(outputpath, Path.GetFileNameWithoutExtension(item)) + ".txt", 
+                    result.ToArray());
+            }
         }
     }
 }
