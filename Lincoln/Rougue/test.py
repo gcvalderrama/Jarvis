53bbd49ef82e9aeed78b835f7861bc6991c708c2
Gregory Valderrama <gvalderrama@verizon.com>
2017-01-22 18:17:24 -0500

    add

--- Lincoln/Rougue/test.py
+++ Lincoln/Rougue/test.py
@@ -7,7 +7,7 @@ import xlsxwriter
 ROUGE = './RELEASE-1.5.5/ROUGE-1.5.5.pl'
 DATA_PATH = './RELEASE-1.5.5/data'
 write_excel = True
-workbook = xlsxwriter.Workbook('rouge_final_conceptual.xlsx')
+workbook = xlsxwriter.Workbook('rouge_final_conceptualbest.xlsx')
 
 
 def excel_init(name):
@@ -181,10 +181,10 @@ def rouge_files_test():
 # lemmatizing_files()
 write_excel = True
 
-rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualNoExpantionSymmaryBest30/', 'ConceptualNoExpantionSummaries')
-rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualSymmaryBest30/', 'ConceptualSummaries')
-rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/RSTNoExpantionSummary/', 'RSTNoExpantion')
-rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/RSTSummary/', 'RST')
+rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualNoExpantionSymmary/', 'ConceptualNoExp')
+rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualNoExpantionSymmaryBest30/', 'ConceptualNoExpBest')
+rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualSymmary30/', 'Conceptual')
+rouge_v2('/Users/gregory/Documents/Github/Jarvis/Final/Training/09ManualSummaries/*.txt', '../LAB/Final/ConceptualSymmaryBest30/', 'ConceptualBest')
 
 
 if write_excel:
